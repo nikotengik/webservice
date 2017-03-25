3888bcc8db6e96cc1ddf848ce574e99e071a0de0
nikotengik <nikotengik@yahoo.com>
2017-03-25 13:36:00 +0700

    Update clientmhsw.php

--- tugas3/clientmhsw.php
+++ tugas3/clientmhsw.php
@@ -15,7 +15,7 @@ if ($err) {
 }
 // Call the SOAP method
 //$result = $client->call('ambilnama', array('name' => 'Unisbank'));
-$result = $client->call('insertdata', array('nim'=> '15.01.53.0041','nama'=> 'febriawan nico','prodi'=> 'TI-C1'));
+$result = $client->call('insertdata', array('nim'=> '15.01.53.0047','nama'=> 'febriawan nico','prodi'=> 'TI-C1'));
 // Check for a fault
 if ($client->fault) {
     echo '<h2>Fault</h2><pre>';
